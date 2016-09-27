d98af377002c9b35c7a4ac426cc102e70d48bc48
Kaden Liang Zhi Phua <kadenphua@gmail.com>
2016-09-27 23:41:38 +0800

    Settled win and draw logic

--- js/script.js
+++ js/script.js
@@ -6,20 +6,33 @@ function init () {
   // var winningCombi = [[][][]]
   var currentPlayer = 1
 
-  var boxes = document.querySelectorAll('.box')
+  var imgs = document.getElementsByClassName('box'),
+  conditions = true,
+  p1 = document.getElementById("p1"),     // Call the paragraph fields with id
+  p2 = document.getElementById("p2"),
+  p3 = document.getElementById("p3"),
+  p4 = document.getElementById("p4"),
+  p5 = document.getElementById("p5"),
+  p6 = document.getElementById("p6"),
+  p7 = document.getElementById("p7"),
+  p8 = document.getElementById("p8"),
+  p9 = document.getElementById("p9");
 
-  for (var i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('click', boxClick)
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener('click', boxClick)
   }
 
   function boxClick () {
     if (currentPlayer === 1) {
-      this.classList.add('clickx')
+      this.classList.add('x');
+      checkWin();
+      checkDraw();
     } else {
-      this.classList.add('clicko')
+      this.classList.add('o');
+      checkWin();
+      checkDraw();
     }
-
-    switchPlayer()
+    switchPlayer();
   }
 
   function switchPlayer () {
@@ -29,4 +42,45 @@ function init () {
       currentPlayer = 1
     }
   }
+}
+
+function checkWin() {
+  if ((p1.classList.contains('x')) && (p2.classList.contains('x')) && (p3.classList.contains('x')) ||
+  ((p4.classList.contains('x')) && (p5.classList.contains('x')) && (p6.classList.contains('x'))) ||
+  ((p7.classList.contains('x')) && (p8.classList.contains('x')) && (p9.classList.contains('x'))) ||
+  ((p1.classList.contains('x')) && (p4.classList.contains('x')) && (p7.classList.contains('x'))) ||
+  ((p2.classList.contains('x')) && (p5.classList.contains('x')) && (p8.classList.contains('x'))) ||
+  ((p3.classList.contains('x')) && (p6.classList.contains('x')) && (p9.classList.contains('x'))) ||
+  ((p1.classList.contains('x')) && (p5.classList.contains('x')) && (p9.classList.contains('x'))) ||
+  ((p7.classList.contains('x')) && (p5.classList.contains('x')) && (p3.classList.contains('x')))) {
+    alert('X is the Winner');
+    reStart();
+    }
+  if ((p1.classList.contains('o')) && (p2.classList.contains('o')) && (p3.classList.contains('o')) ||
+    ((p4.classList.contains('o')) && (p5.classList.contains('o')) && (p6.classList.contains('o'))) ||
+    ((p7.classList.contains('o')) && (p8.classList.contains('o')) && (p9.classList.contains('o'))) ||
+    ((p1.classList.contains('o')) && (p4.classList.contains('o')) && (p7.classList.contains('o'))) ||
+    ((p2.classList.contains('o')) && (p5.classList.contains('o')) && (p8.classList.contains('o'))) ||
+    ((p3.classList.contains('o')) && (p6.classList.contains('o')) && (p9.classList.contains('o'))) ||
+    ((p1.classList.contains('o')) && (p5.classList.contains('o')) && (p9.classList.contains('o'))) ||
+    ((p7.classList.contains('o')) && (p5.classList.contains('o')) && (p3.classList.contains('o')))) {
+      alert("O is the Winner");
+      reStart();
+    }
+  }
+
+function checkDraw () {
+  if ((p1.classList.contains('x') || p1.classList.contains('o')) &&
+  (p2.classList.contains("x") || p2.classList.contains("o")) &&
+  (p3.classList.contains("x") || p3.classList.contains("o")) &&
+  (p4.classList.contains("x") || p4.classList.contains("o")) &&
+  (p5.classList.contains("x") || p5.classList.contains("o")) &&
+  (p6.classList.contains("x") || p6.classList.contains("o")) &&
+  (p7.classList.contains("x") || p7.classList.contains("o")) &&
+  (p8.classList.contains("x") || p8.classList.contains("o")) &&
+  (p9.classList.contains("x") || p9.classList.contains("o"))
+) {
+  alert('It is a Draw!')
+  reStart();
+}
 }
